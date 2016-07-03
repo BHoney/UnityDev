4077364325f6d05a36dddcadd98f3f2c655574dc
BloodyHoney <jstc.14@gmail.com>
2016-07-03 08:38:02 -0400

    Updating Blockbreaker

--- Block Breaker/Assets/Scripts/Ball.cs
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -4,12 +4,31 @@ using System.Collections;
 public class Ball : MonoBehaviour {
 	// Use this for initialization
 	public Paddle paddle;
+	private Vector3 paddleToBallDist;
+	private Rigidbody2D rb2d;
+	private bool isStarted;
 	void Start () {
-	
+		paddleToBallDist = this.transform.position - paddle.transform.position;
+		rb2d = GetComponent<Rigidbody2D>();
+		isStarted = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log(paddleToBallDist);
+		if(!isStarted){
+		this.transform.position = paddle.transform.position + paddleToBallDist;
+		if(Input.GetMouseButtonDown(0)){
+			Debug.Log("Clicked.");
+				isStarted = true;
+				rb2d.velocity = new Vector2((paddle.transform.position.x/5), 12.75f);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collider){
+		if (collider.gameObject.name == ("Paddle"))
+		{
+		}
 	}
 }
