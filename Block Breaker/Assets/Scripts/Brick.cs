db26e6b5cb09f3f0629f92cf9b10b1ef94a85f1a
BloodyHoney <jstc.14@gmail.com>
2016-07-06 17:26:51 -0400

    Added switch statement to ensure bricks change color when hit.

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -1,28 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Brick : MonoBehaviour {
-	public int maxHits;
-	private int hits;
-	private LevelManager lm;
-	// Use this for initialization
-	void Start () {
-		hits = 0;
-		lm = GameObject.FindObjectOfType<LevelManager>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//Debug.Log(hits);
-		if(hits == maxHits){Destroy(gameObject);}
-	}
+public class Brick : MonoBehaviour
+{
+    public int maxHits;
+    private int hits;
+    private LevelManager lm;
+    private SpriteRenderer sprite;
+    // Use this for initialization
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        SetColor(maxHits);
+    }
 
-	void OnCollisionEnter2D(Collision2D collider)
-	{hits++;}
-		//SimulateWin();
+    void Start()
+    {
+        hits = 0;
+        lm = GameObject.FindObjectOfType<LevelManager>();
 
-	//TODO: Remove this once we have real win conditions
-	void SimulateWin(){lm.loadNextLevel();
-	}
-		
+    }
+
+    void SetColor(int hits)
+    {
+        switch (hits)
+        {
+            case 3:
+                sprite.color = Color.green;
+                break;
+            case 2:
+                sprite.color = Color.yellow;
+                break;
+            default:
+                sprite.color = Color.red;
+                break;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(hits);
+        if (hits == maxHits) { Destroy(gameObject); }
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        hits++;
+        SetColor(maxHits - hits);
+    }
+    //SimulateWin();
+
+    //TODO: Remove this once we have real win conditions
+    void SimulateWin()
+    {
+        lm.loadNextLevel();
+    }
+
 }
