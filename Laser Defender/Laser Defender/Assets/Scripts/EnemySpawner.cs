4c15d0f9c2e3030b9abb6a51d2abb2b1e7cd2780
BloodyHoney <jstc.14@gmail.com>
2016-08-12 23:46:33 -0400

    Added basic enemy and player movement.

---
+++ Laser Defender/Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public enum Direction { Left, Right };
+    public float width;
+    public float height;
+    private bool moveRight;
+    public float movespeed = 1f;
+	public float xmin, xmax;
+    // Use this for initialization
+    void Start()
+    {
+		moveRight = (Random.Range(0,2) == 0);
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = (Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance)));
+		Vector3 rightmost = (Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance)));
+		xmin = leftmost.x;
+		xmax = rightmost.x;
+        foreach (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.SetParent(child);
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector2(width, height));
+    }
+
+    // Update is called once per frame
+	void Update(){
+		
+		float leftEdge = transform.position.x - width*.5f;
+		float rightEdge = transform.position.x + width*.5f;
+		if(leftEdge < xmin) moveRight = true;
+		else if (rightEdge > xmax) moveRight = false;
+
+
+		 //float boundaries = Mathf.Clamp(transform.position.x, xmin, xmax);
+		 //transform.position = new Vector2(boundaries, transform.position.y);
+	}
+
+
+    void FixedUpdate()
+    {
+        float boundaries = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector2(boundaries, transform.position.y);
+        if (moveRight) move(Direction.Right);
+        else move(Direction.Left);
+    }
+
+    void move(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Left:
+                transform.position -=new Vector3(movespeed * Time.deltaTime, 0);
+                break;
+            case Direction.Right:
+                transform.position += new Vector3(movespeed * Time.deltaTime, 0);
+                break;
+            default:
+                return;
+        }
+    }
+}
--- Laser Defender/Laser Defender/Assets/Scripts/playerController.cs
+++ Laser Defender/Laser Defender/Assets/Scripts/playerController.cs
@@ -3,30 +3,43 @@ using System.Collections;
 
 public class playerController : MonoBehaviour {
 	public enum Direction {Left, Right};
-	public float moveSpeed = 5f;
+	public float moveSpeed;
 	private float moveH = 0f;
+	public float padding = .5f;
+	float xmin;
+	float xmax;
 	// Use this for initialization
 	void Start () {
-
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+		Vector2 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0, distance));
+		xmin=leftMost.x+padding;
+		xmax=rightMost.x-padding;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if(Input.GetKey(KeyCode.LeftArrow)) move(Direction.Left);
 		if(Input.GetKey(KeyCode.RightArrow)) move(Direction.Right);
-		gameObject.transform.position = new Vector2(moveH, 0);
+		gameObject.transform.position = new Vector2(moveH*Time.deltaTime, transform.position.y);
+		
 
+	}
+
+	void Update(){
+		float boundaries = Mathf.Clamp(transform.position.x, xmin, xmax);
+		transform.position = new Vector2(boundaries, transform.position.y);
 	}
 
 	void move(Direction direction){
 		switch (direction)
 		{
 			case Direction.Left:
-				moveH--;
+				moveH-=moveSpeed;
 			break;
 
 			case Direction.Right:
-				moveH++;
+				moveH+=moveSpeed;
 			break;
 			default:
 			return;
