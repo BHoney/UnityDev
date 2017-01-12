d74c102355986f78ccb8fbca6349986076fff792
BloodyHoney <jstc.14@gmail.com>
2017-01-12 11:06:41 -0500

    Back on the saddle

---
+++ Glitch Gardens/Glitch Gardens/Assets/Scripts/TransitionElement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionElement : MonoBehaviour {
+
+		float time;
+		float transition;
+		public LevelManager levelmanager;
+	// Use this for initialization
+	void Start () {
+		time = Time.time;
+		transition = time + 5;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+			if(time <= transition){
+				levelmanager.LoadLevel("Start Menu");
+			}
+		}
+	}
